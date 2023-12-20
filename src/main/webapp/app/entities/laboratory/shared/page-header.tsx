36df920b270f04438100776c8dd7b399ed615796
DevPachon <devpachon@outlook.com>
2023-12-20 15:43:12 -0500

    Fixed action of header, changed to position absolute

--- src/main/webapp/app/entities/laboratory/shared/page-header.tsx
+++ src/main/webapp/app/entities/laboratory/shared/page-header.tsx
@@ -5,7 +5,7 @@ import { capitalizeFirstLetter } from 'app/shared/util/string-utils';
 const PageHeader = ({ title, rightAction = null, leftAction = null }) => (
   <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static" sx={{ mb: 2, p: 1, flexDirection: 'row' }}>
-      {leftAction && <Box sx={{ flexGrow: 1, placeSelf: 'end' }}>{leftAction}</Box>}
+      {leftAction && <Box sx={{ flexGrow: 1, placeSelf: 'end', position: 'absolute' }}>{leftAction}</Box>}
       <Typography variant="h5" component="div" align="center" sx={{ flexGrow: 20 }}>
         {capitalizeFirstLetter(title)}
       </Typography>
